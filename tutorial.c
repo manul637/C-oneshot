1c7dd14ce4eb314d640003edb8b888aa263ed42f
manul <manulgupta1009@gmail.com>
2025-01-05 20:56:14 +0530

    lecture 1

---
+++ tutorial.c
@@ -0,0 +1,37 @@
+ # include<stdio.h>
+
+//  int main() {
+//     printf("hello world !");
+//     return 0;
+//  }
+
+// area of square 
+
+// int main () {
+//     int side;
+//     printf("enter side \n");
+//     scanf("%d",&side);
+
+//     printf("are of square is %d", side*side);
+//     return 0;
+// }
+
+// area of circle
+
+
+// int main()
+// {
+
+// float pi = 3.14;
+// float rad;
+// printf("enter radius");
+// scanf("%f", &rad);
+
+// printf("area of circle is %f" , pi*rad*rad);
+// return 0;
+
+// }
+
+
+
+
