ca84e91cd4822ea8bbd22e612ab9252f4bfe2a60
manul <manulgupta1009@gmail.com>
2025-01-06 13:29:32 +0530

    lecture 2

--- tutorial.c
+++ tutorial.c
@@ -34,4 +34,54 @@
 
 
 
+// WAP to check if a number is divisible or not .
 
+
+// int main() {
+//     int x;
+//     printf("enter number");
+//     scanf("%d",&x);
+
+//     printf("%d" , x % 2 == 0);
+//     return 0;
+
+// }
+
+
+
+// if its sunday and its snowing -> true
+
+
+
+// int main() {
+//     int isSunday = 1;
+//     int isSnowing =1;
+
+//     printf("%d \n", isSunday && isSnowing);
+
+//     return 0;
+// }
+
+
+// int main () {
+//     int x;
+//     printf("enter number");
+//     scanf("%d", &x);
+
+//     printf("%d \n" , x>9 && x<100 );
+//     return 0 ;
+
+// }
+
+
+
+int main() {
+    int a , b;
+    printf("enter numbers : ");
+    scanf("%d %d " ,&a ,b);
+
+    printf(" %d  " , a<b);
+    return 0;
+
+
+}
