31ac04a451a9b433b095f4468c27dca29e13336f
manul <manulgupta1009@gmail.com>
2025-01-08 09:00:51 +0530

    lecture 4

--- lecture4.c
+++ lecture4.c
@@ -1,8 +1,183 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(){
-    for(i=0; i<5; i++){
-        printf("manul \n");
+
+// FOR LOOP
+
+
+// int main(){
+//     for( int i=0; i<5; i++){
+//         printf("manul \n");
+//     }
+//     return 0;
+// }
+
+
+// PRINT NUMBERS 1 - 100t
+
+// int main (){
+
+//     for (int i=0 ; i <100 ; i ++ ){
+//         printf("%d\n" , i+1);
+//     }
+//     return 0;
+// }
+
+
+
+// ITERATOR CAN BE FLOAT AND CHARACTER 
+
+
+// int main () {
+//     for(float i = 1.0; i <=10.0 ;i++ ){
+//         printf("%f \n", i);
+//     }
+//     return 0;
+// }
+
+
+// int main () {
+//     for(char ch = 'A' ; ch <= '*' ; ch++){
+//         printf("%c \n" , ch);
+//     }
+//     return 0;
+// }
+
+// DO WHILE LOOP
+
+// int main (){
+//     int i = 1;
+//     do {
+//         printf("%d \n" , i);
+//         i++;
+//     } while (i>10);
+
+//     return 0 ;
+    
+// }
+
+
+
+// SUM OF FIRST N NATURAL NUMBER .
+
+
+// int main () {
+//     int n;
+//     int sum = 0;
+
+//     printf("enetr number : \n");
+//     scanf("%d" , &n);
+
+//     for(int i=1 ; i<=n ; i++){
+//         printf(" sum is %d \n" , sum=sum+i);     [       another way is in do something you just sum and
+//                                                                        print outside the loop    ]
+//     }
+//     return 0;
+// }
+
+
+// for printing them in reverse                             [ GOOD QUESTION ] CAN BE DONE ONLY BY ONE ITERATOR 
+
+// for ( int i =n ; i>=n ; i--){
+//     printf("%d \n" , i);
+// }
+
+
+
+// for printinng table of a number
+
+
+// int main () {
+//     int n;
+//     printf(" enter number :");
+//     scanf("%d", &n);
+
+//     for(int i=1 ; i<=10 ; i++){
+//         printf("%d \n" , n*i) ;
+//     }
+//     return 0 ;
+// }
+
+
+// USE OF BREAK 
+
+
+// int main () {
+//     int n;
+//     do {
+//         printf("enter number :");
+//         printf("%d \n" ,n );
+
+//         if(n % 2 != 0){
+//             break;
+//         }
+//     } while (1)
+
+//     return 0;
+
+// }
+
+
+
+
+// PRITN ALL ODD NO FROM 5 - 50 
+
+
+// int main() {
+//     for( int i = 5 ; i< 50 ; i++){
+//         if( i %2 != 0){
+//             printf("%d \n" , i);
+//         }
+//     }
+//     return 0;
+// }
+
+
+
+// int main () {
+
+//     int n;
+//     printf("enter number : ");
+//     scanf("%d " , &n);
+    
+//     int fact = 1;
+
+//     for (int i =1 ; i <= n ; i++){
+//         fact = fact *i;
+//     }
+//     printf("%d" , fact);
+
+//     return 0;
+// }
+
+
+// print table in reverse 
+
+
+// int main () {
+
+//     int n;
+//     printf(" enter number: ");
+//     scanf("%d", &n);
+
+//     for(int i =10 ; i>=1 ; i--){
+//         printf("%d \n", n*i);
+
+//     }
+//      return 0;
+// }
+
+
+// CALCULATE THE SUM BETWEEN 5 AND 50 INCLUDING 
+
+
+int main (){
+
+    int sum = 0;
+    for(int i =5 ; i<=50 ; i++){
+        sum = sum +i;                                   //sum =+ i
     }
+    printf(" sum is : %d \n" , sum);
+
     return 0;
+
 }
