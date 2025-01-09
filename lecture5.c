8d665f5747b7bf252a98166881d6fec6fc70e908
manul <manulgupta1009@gmail.com>
2025-01-09 23:46:44 +0530

    add lecture 6

--- lecture5.c
+++ lecture5.c
@@ -195,26 +195,26 @@
 // FABIONACCI NUMBER BY RECURSSION 
 
 
-int fab(int n);
+// int fab(int n);
 
-int main(){
-    fab(6);
+// int main(){
+//     fab(6);
 
-    return 0;
-}
+//     return 0;
+// }
 
-int fab(int n){
-    if(n==0){
-        return 0;
+// int fab(int n){
+//     if(n==0){
+//         return 0;
     
-    }
-    if(n==1){
-        return 1;
-    }
+//     }
+//     if(n==1){
+//         return 1;
+//     }
 
-    int fabNm1 = fab(n-1);
-    int fabNm2 = fab(n-2);
-    int fabN = fabNm1 + fabNm2;
-    printf("fab of %d is %d " , n ,fabN);
-    return fabN;
-}
+//     int fabNm1 = fab(n-1);
+//     int fabNm2 = fab(n-2);
+//     int fabN = fabNm1 + fabNm2;
+//     printf("fab of %d is %d " , n ,fabN);
+//     return fabN;
+// }
